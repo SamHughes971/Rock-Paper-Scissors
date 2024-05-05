e1ed887429f12bc875788a11b7cd25a3e594b54d
Samuel Hughes <samuel971@hotmail.co.uk>
2024-05-05 21:35:02 +0100

    Remove unnecessary comments. This change is due to 'The Odin Project' - Clean Code)

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-// INITIAL STEPS FOR ROCK, PAPER SCISSORS GAME (OVERVIEW)
-
-// 1) THE CONSOLE ASKS THE USER ROCK, PAPER, SCISSORS
-
-// 2) STORE USER CHOICE INSIDE OF VARIABLE CALLED userChoice 
-
-// 3) COMPUTER RANDOMLY CHOOSES ROCK, PAPER, SCISSORS
-
-// 4) STORE COMPUTER CHOICE INSIDE OF VARIABLE 
-// CALLED computerChoice
-
-// 5) COMPARE computerChoice to userChoice
-
-// 6) IF THE COMPUTER WINS, DISPLAY MESSAGE 'COMPUTER WON. 
-// IF USER WINS, DISPLAY MESSAGE 'USER WON'
-
-
 // Generate Computer's choice
 
 function getComputerChoice(){
@@ -40,7 +23,6 @@ function getComputerChoice(){
 // Generate user's choice
 
 function getHumanChoice(){
-    //humanInput ask user to enter prompt (string);
     const humanInput = prompt("rock, paper, scissors?: ")
     // humanDesicion transforms humanInput to uppercase (toUpperCase()) to make
     // everything equal
@@ -56,13 +38,8 @@ function getHumanChoice(){
         return console.log("Invalid operation. Please try again");
     }
 }
-
-// Declare score variables
-
 let humanScore = 0;
 let computerScore = 0;
-
-//Create playRound function
 
 function playRound(humanSelection, computerSelection){
     
